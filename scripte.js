c2e67a35380c805dfb463c0c3c1db62c02bd9ff9
AnthonyVanDeyck <anthonyvandeyck@gmail.com>
2022-06-22 09:48:43 +0200

    modif js

--- scripte.js
+++ scripte.js
@@ -4,7 +4,7 @@ var content = document.getElementById('content');
 var top = document.getElementById('top');
 var table = document.getElementById('table1');
 var graphCanvas = document.createAttribute('canvas');
-graphCanvas.setAttribute('iod', graphId);
+graphCanvas.setAttribute('id', graphId);
 graphCanvas.setAttribute('width', graphWidth);
 graphCanvas.setAttribute('height', graphHeight);
 table.prepend(graphCanvas);
